131d5c6ca0d632e8ee52fe2c76bf714d520d9d17
Isaac Dias <isaacardias@gmail.com>
2018-10-09 21:25:19 -0300

    add datetime

---
+++ datetime.py
@@ -0,0 +1,26 @@
+# coding: UTF-8
+
+#
+# Datas
+#
+
+from datetime import datetime
+now = datetime.now()
+
+print(now)
+print(now.year)
+print(now.month)
+print(now.day)
+
+#
+# Para imprimir a data no formato brasileiro
+#
+
+print('%s/%s/%s' %(now.day, now.month, now.year))
+
+#
+# Imprimindo as horas
+#
+
+print('%s:%s:%s' %(now.hour, now.minute, now.second))
+
