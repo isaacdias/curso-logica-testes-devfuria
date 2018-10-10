8db9ee6cf0f4b52ff64b381e1b16c61d965bc018
Isaac Dias <isaacardias@gmail.com>
2018-10-09 21:26:39 -0300

    add import

---
+++ import.py
@@ -0,0 +1,18 @@
+# coding: UTF-8
+
+#
+# imports
+#
+
+# Utilizamos o import para importar um modulo
+
+import math
+print(math.sqrt(25))
+
+# O codigo acima importará todos os modulos de math.
+
+# Para importar apenas o necessário utilizamos from
+
+from math import sqrt
+
+# 
