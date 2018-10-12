4c24a9f180da639994c35f71e31cdf8a7ef0f83e
Isaac Dias <isaacardias@gmail.com>
2018-10-12 16:00:17 -0300

    add exercicio area quadrada

---
+++ area_quadrada.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+
+#
+# função que retorna a area quadr
+#
+
+def area_quadrada(comprimento, largura):
+	return comprimento * largura
+
+#
+# teste
+#
+
+assert 12 == area_quadrada(3, 4)
