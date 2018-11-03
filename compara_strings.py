2530a8a67c58ea9f219fa66c889dc6a772491d26
Isaac Dias <isaacardias@gmail.com>
2018-11-03 16:34:10 -0300

    add exercicio que comprar strings

---
+++ compara_strings.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+
+string1 = 'python'
+string2 = 'python'
+
+#
+# retorna se duas strgins são iguais
+#
+
+def comparaString(string1, string2):
+    return string1 == string2
+
+#
+# teste
+#
+
+assert True == comparaString(string1, string2)
+
+
+
